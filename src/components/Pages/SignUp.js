0d82564489fd21ab3c81ba0c6bf6d022cc75686f
Nebajooo <nebajoo2015@gmail.com>
2024-02-14 23:12:52 -0800

    update in login form of the project website

--- src/components/Pages/SignUp.js
+++ src/components/Pages/SignUp.js
@@ -1,6 +1,30 @@
 import React from "react";
 import "../../App.css";
-
+import "./SignUp.css";
+import { NavLink, Link } from "react-router-dom";
+import Footer from "../Footer/Footer";
 export default function SignUp() {
-  return <h1 className="sign-up">SIGN UP</h1>;
+  return (
+    <div>
+      <div class="main"></div>
+      <div class="form-box">
+        <form class="form">
+          <span class="title">Sign up</span>
+          <span class="subtitle">Create a free account with your email.</span>
+          <div class="form-container">
+            <input type="text" class="input" placeholder="Full Name" />
+            <input type="email" class="input" placeholder="Email" />
+            <input type="password" class="input" placeholder="Password" />
+          </div>
+          <button>Sign up</button>
+        </form>
+        <div class="form-section">
+          <p>
+            Have an account? <a href="">Log in</a>{" "}
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
